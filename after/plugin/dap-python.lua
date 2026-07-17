97d6c9a4971ead9a6b065ca77535e17110d7d66f
Bryan Tabares <tabaresbryan39@gmail.com>
2026-07-17 19:12:47 +0800

    fix: broken treesitter

--- after/plugin/dap-python.lua
+++ after/plugin/dap-python.lua
@@ -1,1 +1,7 @@
-require("dap-python").setup("./venv/bin/python")
+local root = vim.fs.dirname(
+    vim.fs.find(".git", {
+        upward = true,
+        type = "directory",
+    })[1]
+)
+require("dap-python").setup(root .. "/venv/bin/python")
