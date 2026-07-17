2f5ab65b117121f50865abf5f24a0d1e33d91712
Bryan Tabares <tabaresbryan39@gmail.com>
2026-07-17 22:21:35 +0800

    chore: migrated to Lazy.nvim

--- after/plugin/dap-python.lua
+++
@@ -1,7 +0,0 @@
-local root = vim.fs.dirname(
-    vim.fs.find(".git", {
-        upward = true,
-        type = "directory",
-    })[1]
-)
-require("dap-python").setup(root .. "/venv/bin/python")
---
+++ lua/plugins/defaults.lua
@@ -0,0 +1,10 @@
+return {
+    {'theprimeagen/vim-be-good', event="VeryLazy"},
+    {'nvim-treesitter/nvim-treesitter-context', event="VeryLazy"},
+    {'stevearc/dressing.nvim'},
+    {'nvim-lua/plenary.nvim'},
+    {'MunifTanjim/nui.nvim'},
+    {'hrsh7th/nvim-cmp'},
+    {'nvim-tree/nvim-web-devicons'},
+    {'HakonHarnes/img-clip.nvim'},
+}
