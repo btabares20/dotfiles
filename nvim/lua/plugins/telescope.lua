fba79ac9447e4dca851026b7114745bba35e6f21
Bryan Tabares <tabaresbryan39@gmail.com>
2026-07-31 01:06:08 +0800

    feat: added worktree on my workflow

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,11 +6,19 @@ return {
         },
         config = function()
             local builtin = require('telescope.builtin')
+            local telescope = require("telescope").extensions.git_worktree
             vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
             vim.keymap.set('n', '<C-p>', builtin.git_files, {})
             vim.keymap.set('n', '<leader>ps', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ")});
             end)
+            vim.keymap.set("n", "<leader>gw", telescope.git_worktrees, {
+                desc = "Git Worktrees",
+            })
+
+            vim.keymap.set("n", "<leader>gW", telescope.create_git_worktree, {
+                desc = "Git Create Worktree",
+            })
         end,
     },
 }
