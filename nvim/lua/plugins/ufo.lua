d84b078c3ea15bdce36d2fe1a1dc8bc5440b93ca
Bryan Tabares <tabaresbryan39@gmail.com>
2026-08-01 04:56:44 +0800

    chore: installed termtoggle and custom fold symbols

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -1,5 +1,36 @@
 return {
     {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+            local builtin = require("statuscol.builtin")
+
+            require("statuscol").setup({
+                relculright = true,
+
+                segments = {
+                    {
+                        text = { builtin.foldfunc },
+                        click = "v:lua.ScFa",
+                    },
+                    {
+                        text = { "%s" },
+                        click = "v:lua.ScSa",
+                    },
+                    {
+                        text = { builtin.lnumfunc, " " },
+                        click = "v:lua.ScLa",
+                    },
+                },
+            })
+            vim.opt.fillchars = {
+                foldopen = "",
+                foldclose = "",
+                fold = " ",
+                foldsep = "│",
+            }
+        end,
+    },
+    {
         'kevinhwang91/nvim-ufo',
         dependencies = {
             'kevinhwang91/promise-async'
